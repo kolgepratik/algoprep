6f9e79729e39a3e7fde4ca6c6d3d871544c4b079
kolgepratik <kolge.pratik@gmail.com>
2018-10-31 22:28:49 -0400

    completed range_sum_query

--- Prep/dp/range_sum_query.js
+++ Prep/dp/range_sum_query.js
@@ -1,23 +1,38 @@
+/*
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+Seen this question in a real interview before?
+*/
+
 /**
  * @param {number[]} nums
  */
 var NumArray = function(nums) {
     this.nums = nums;
 
-    this.sum_range_dp = compute_dp_array (nums);
-
-       
+    this.sum_array = compute_sum_array (nums);    
+    
+    console.dir (this.sum_array);
 };
 
-function compute_dp_array (nums) {
-    let dp_array = new Array (nums.length);
-    for (let e=0; e<sum_range_dp.length; e++) {
-        sum_range_dp.push (new Array(nums.length));
+function compute_sum_array (nums) {
+    let sum_array = new Array (nums.length);
+    
+    sum_array [0] = nums[0];
+    for (let e=1; e<nums.length; e++) {
+        sum_array[e] = nums[e] + sum_array[e-1];
     }
 
-    
-
-    return dp_array;
+    return sum_array;
 }
 
 /** 
@@ -26,11 +41,21 @@ function compute_dp_array (nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(i, j) {
-    
+    if (i < 0) {
+        return 0;
+    } else if (i === 0) {        
+        return this.sum_array [j];
+    } else {
+        return this.sum_array [j] - this.sum_array [i-1];
+    }
 };
 
 /** 
  * Your NumArray object will be instantiated and called as such:
- * var obj = Object.create(NumArray).createNew(nums)
+ * var obj = Object.create(NumArray).createNew([-2, 0, 3, -5, 2, -1])
  * var param_1 = obj.sumRange(i,j)
  */
+
+var obj = new NumArray ([-2, 0, 3, -5, 2, -1]);
+var param_1 = obj.sumRange(2, 5);
+console.log (`paran_1 = ${param_1}`);
