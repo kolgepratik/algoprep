f5f7fb70f8ca76e26f3a46a91a845e7098a4f437
kolgepratik <kolge.pratik@gmail.com>
2018-11-06 15:01:28 -0500

    added min-falling-cost

--- Prep/dp/min-falling-path-sum.js
+++ Prep/dp/min-falling-path-sum.js
@@ -29,5 +29,47 @@ Note:
  * @return {number}
  */
 var minFallingPathSum = function(A) {
-    
+    if (A === undefined || A.length === undefined || A.length === 0 || A[0] === undefined || A[0].length === undefined || A[0].length !== A.length) {
+        return 0;
+    } else if (A.length === 1) {
+        return A[0][0];
+    } else {
+        let square_dimension = A.length;
+
+        let min_sum_dp = new Array (square_dimension);
+        for (let r=0; r<square_dimension; r++) {
+            min_sum_dp[r] = new Array (square_dimension);
+
+            // Set values for 1st row. 
+            min_sum_dp[0][r] = A[0][r];
+        }
+
+        let row_min = undefined;
+        for (let r=1; r<square_dimension; r++) {
+            row_min = Number.MAX_SAFE_INTEGER;
+
+            for (let c=0; c<square_dimension; c++) {
+                let left = (c === 0) ? Number.MAX_SAFE_INTEGER : min_sum_dp[r-1][c-1];
+                let middle = min_sum_dp[r-1][c];
+                let right = (c === (square_dimension - 1)) ? Number.MAX_SAFE_INTEGER : min_sum_dp[r-1][c+1];
+
+                min_sum_dp[r][c] = A [r][c] 
+                    + Math.min (
+                        left, middle, right
+                    );
+
+                row_min = Math.min (row_min, min_sum_dp[r][c]);
+            }
+        } 
+
+        return row_min;
+    }
 };
+
+let A = [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+];
+
+console.log (`get_min_sum = ${minFallingPathSum (A)}`);
