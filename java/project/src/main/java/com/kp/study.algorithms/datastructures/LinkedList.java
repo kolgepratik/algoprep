1557996f1dbfa00f8b93bb41905c3313918cf1ce
kolgepratik <kolge.pratik@gmail.com>
2019-06-25 22:58:03 -0400

    Add LinkedList.swap()

--- java/project/src/main/java/com/kp/study.algorithms/datastructures/LinkedList.java
+++ java/project/src/main/java/com/kp/study.algorithms/datastructures/LinkedList.java
@@ -2,6 +2,7 @@ package com.kp.study.algorithms.datastructures;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class LinkedListNode {
     int data;
@@ -60,8 +61,17 @@ public class LinkedList {
         return 1 + this.sizeRecursiveHelper(currentNode.next);
     }
 
-    LinkedListNode getLast() {
+    LinkedListNode getLastNode() {
         return this.getLastHelper(this.head);
+    }
+
+    LinkedListNode getNodeAtPosition(int index) {
+        LinkedListNode nodePointer = this.head;
+
+        for (int i = 1; nodePointer != null && i < index; i++)
+            nodePointer = nodePointer.next;
+
+        return nodePointer;
     }
 
     private LinkedListNode getLastHelper(LinkedListNode nodePointer) {
@@ -69,5 +79,96 @@ public class LinkedList {
             return nodePointer;
 
         return this.getLastHelper(nodePointer.next);
+    }
+
+    void swapNodes(int firstNodeData, int secondNodeData) {
+        if (this.head != null && (firstNodeData != secondNodeData)) {
+            LinkedListNode first = this.head;
+            LinkedListNode firstPrev = null;
+
+            while (first != null) {
+                if (first.data == firstNodeData) {
+                    break;
+                }
+
+                firstPrev = first;
+                first = first.next;
+            }
+
+            LinkedListNode second = this.head;
+            LinkedListNode secondPrev = null;
+            while (second != null) {
+                if (second.data == secondNodeData) {
+                    break;
+                }
+
+                secondPrev = second;
+                second = second.next;
+            }
+
+            if (first != null && second != null) {
+                if (firstPrev == null) {
+                    second.next = first.next;
+                    this.head = second;
+
+                    first.next = null;
+                    secondPrev.next = first;
+                }
+                else if (secondPrev == null) {
+                    first.next = second.next;
+                    this.head = first;
+
+                    second.next = null;
+                    firstPrev.next = second;
+                }
+                else {
+                    LinkedListNode firstNext = first.next;
+                    LinkedListNode secondNext = second.next;
+                    firstPrev.next = second;
+                    secondPrev.next = first;
+
+                    second.next = firstNext;
+                    first.next = secondNext;
+                }
+
+                System.out.println("List After swapping: " + this);
+            }
+        }
+    }
+
+    public void swapPairwise() {
+        if (this.head != null) {
+            LinkedListNode first = this.head;
+            LinkedListNode second = first.next;
+
+            if (second != null) {
+                this.head = second;
+                first.next = second.next;
+                second.next = first;
+
+                LinkedListNode prev = first;
+                LinkedListNode current = first.next;
+                while(current != null && current.next != null) {
+                    LinkedListNode next = current.next;
+
+                    prev.next = next;
+                    current.next = next.next;
+                    next.next = current;
+
+                    prev = current;
+                    current = current.next;
+                }
+
+                System.out.println("List After swapping: " + this);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        final String allNodes =
+                this.getAll().stream().map(LinkedListNode::toString).collect(Collectors.joining("\n"));
+
+        return "\nLinkedList {" + "head=" + head + "} " + "\nAll Nodes: \n" + allNodes;
     }
 }
--- java/project/src/test/java/com/kp/study/algorithms/datastructures/LinkedListTest.java
+++ java/project/src/test/java/com/kp/study/algorithms/datastructures/LinkedListTest.java
@@ -2,12 +2,16 @@ package com.kp.study.algorithms.datastructures;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 class LinkedListTest {
 
     @Test
-    void testAppend() {
+    void append() {
         LinkedList linkedList = new LinkedList();
         assertNull(linkedList.head);
 
@@ -16,5 +20,126 @@ class LinkedListTest {
 
         assertNotNull(linkedList.head);
         assertEquals(1, linkedList.sizeRecursive());
+    }
+
+    @Test
+    void getNodeAtPosition() {
+        LinkedList linkedList = new LinkedList();
+
+        appendItems(linkedList, 1, 5, integer -> integer + 1);
+        Assumptions.assumeTrue(5 == linkedList.sizeRecursive());
+
+        // Act
+        LinkedListNode first = linkedList.getNodeAtPosition(1);
+        assertNotNull(first);
+        assertEquals(1, first.data);
+
+        LinkedListNode second = linkedList.getNodeAtPosition(2);
+        assertNotNull(second);
+        assertEquals(2, second.data);
+
+        LinkedListNode third = linkedList.getNodeAtPosition(3);
+        assertNotNull(third);
+        assertEquals(3, third.data);
+
+        LinkedListNode fourth = linkedList.getNodeAtPosition(4);
+        assertNotNull(fourth);
+        assertEquals(4, fourth.data);
+
+        LinkedListNode fifth = linkedList.getNodeAtPosition(5);
+        assertNotNull(fifth);
+        assertEquals(5, fifth.data);
+
+        LinkedListNode tenth = linkedList.getNodeAtPosition(10);
+        assertNull(tenth);
+    }
+
+    @Test
+    void swapNodes() {
+        LinkedList linkedList = new LinkedList();
+
+        appendItems(linkedList, 1, 5, integer -> integer + 1);
+        Assumptions.assumeTrue(5 == linkedList.sizeRecursive());
+
+        Assumptions.assumeTrue(2 == linkedList.getNodeAtPosition(2).data);
+        Assumptions.assumeTrue(4 == linkedList.getNodeAtPosition(4).data);
+
+        // Act
+        linkedList.swapNodes(2, 4);
+
+        assertEquals(4, linkedList.getNodeAtPosition(2).data);
+        assertEquals(2, linkedList.getNodeAtPosition(4).data);
+
+        // Act
+        linkedList.swapNodes(1, 5);
+
+        assertEquals(5, linkedList.getNodeAtPosition(1).data);
+        assertEquals(1, linkedList.getNodeAtPosition(5).data);
+
+        // Act
+        linkedList.swapNodes(2, 4);
+
+        assertEquals(2, linkedList.getNodeAtPosition(2).data);
+        assertEquals(4, linkedList.getNodeAtPosition(4).data);
+
+        // Act
+        linkedList.swapNodes(1, 5);
+
+        assertEquals(1, linkedList.getNodeAtPosition(1).data);
+        assertEquals(5, linkedList.getNodeAtPosition(5).data);
+    }
+
+    @Test
+    void swapPairwise() {
+        LinkedList evenLinkedList = new LinkedList();
+
+        appendItems(evenLinkedList, 1, 6, integer -> integer + 1);
+        Assumptions.assumeTrue(6 == evenLinkedList.sizeRecursive());
+
+        Assumptions.assumeTrue(1 == evenLinkedList.getNodeAtPosition(1).data);
+        Assumptions.assumeTrue(2 == evenLinkedList.getNodeAtPosition(2).data);
+        Assumptions.assumeTrue(3 == evenLinkedList.getNodeAtPosition(3).data);
+        Assumptions.assumeTrue(4 == evenLinkedList.getNodeAtPosition(4).data);
+        Assumptions.assumeTrue(5 == evenLinkedList.getNodeAtPosition(5).data);
+        Assumptions.assumeTrue(6 == evenLinkedList.getNodeAtPosition(6).data);
+
+        // Act
+        evenLinkedList.swapPairwise();
+
+        assertEquals(2, evenLinkedList.getNodeAtPosition(1).data);
+        assertEquals(1, evenLinkedList.getNodeAtPosition(2).data);
+        assertEquals(4, evenLinkedList.getNodeAtPosition(3).data);
+        assertEquals(3, evenLinkedList.getNodeAtPosition(4).data);
+        assertEquals(6, evenLinkedList.getNodeAtPosition(5).data);
+        assertEquals(5, evenLinkedList.getNodeAtPosition(6).data);
+
+        LinkedList oddLinkedList = new LinkedList();
+
+        appendItems(oddLinkedList, 1, 7, integer -> integer + 1);
+        Assumptions.assumeTrue(7 == oddLinkedList.sizeRecursive());
+
+        Assumptions.assumeTrue(1 == oddLinkedList.getNodeAtPosition(1).data);
+        Assumptions.assumeTrue(2 == oddLinkedList.getNodeAtPosition(2).data);
+        Assumptions.assumeTrue(3 == oddLinkedList.getNodeAtPosition(3).data);
+        Assumptions.assumeTrue(4 == oddLinkedList.getNodeAtPosition(4).data);
+        Assumptions.assumeTrue(5 == oddLinkedList.getNodeAtPosition(5).data);
+        Assumptions.assumeTrue(6 == oddLinkedList.getNodeAtPosition(6).data);
+        Assumptions.assumeTrue(7 == oddLinkedList.getNodeAtPosition(7).data);
+
+        // Act
+        oddLinkedList.swapPairwise();
+
+        assertEquals(2, oddLinkedList.getNodeAtPosition(1).data);
+        assertEquals(1, oddLinkedList.getNodeAtPosition(2).data);
+        assertEquals(4, oddLinkedList.getNodeAtPosition(3).data);
+        assertEquals(3, oddLinkedList.getNodeAtPosition(4).data);
+        assertEquals(6, oddLinkedList.getNodeAtPosition(5).data);
+        assertEquals(5, oddLinkedList.getNodeAtPosition(6).data);
+        assertEquals(7, oddLinkedList.getNodeAtPosition(7).data);
+
+    }
+
+    private void appendItems(LinkedList linkedList, int seed, int limit, Function<Integer, Integer> mapper) {
+        Stream.iterate(seed, mapper::apply).limit(limit).forEach(linkedList::append);
     }
 }
